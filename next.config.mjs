11452a3f8499a57c0e0b6c09e901855e81095cf5
alitkovsky <alitkovsky@me.com>
2024-11-19 15:31:15 +0100

    Adding changes for the new version.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
-import nextI18NextConfig from './next-i18next.config.js';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   ...nextI18NextConfig,
    reactStrictMode: true,
 };
 
