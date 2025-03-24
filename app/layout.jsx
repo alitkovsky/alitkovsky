cf3bb18e7bc8e7def2c640ec8cdfdd5925b88d67
alitkovsky <alitkovsky@me.com>
2025-03-24 11:04:50 +0100

    Added the Google Tag Manager.

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import "./media-queries.css";
 
 import Head from "next/head";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import Header from "@/components/Header";
 
@@ -15,13 +16,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <link rel="shortcut icon" type="image/png" href="favicon-32x32.png" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R9C07PPTEZ"></script>
       </Head>
+      <GoogleTagManager gtmId="GTM-N4GKN2G2" />
       <body>
           <div className="isolate">
             <Header />
             {children}
           </div>
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4GKN2G2" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
       </body>
     </html>
   );
